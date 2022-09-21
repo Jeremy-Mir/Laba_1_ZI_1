8c991748403b0c7724df5f5afee15949074976cd
Your Name <elik@example.com>
2022-09-21 11:21:03 +0300

    йцу

--- 1_1.py
+++ 1_1.py
@@ -1,10 +1,10 @@
 from random import randint
 
-f_naw = open('text.txt', 'r')
-naw = "informaticsq"
-w = [["q","w"],["e","r"],["t","y"],["u","i"],["o","p"],["a","s"]]
-h = [["d","f"],["g","h"],["j","k"],["l","z"],["x","c"],["v","b"]]
-alf = "abcdefghijklmnopqrstuvwxyz?!.-_,:; "
+f_naw = open('text.txt', 'r',encoding='utf-8')
+naw = input("Введите: ")
+w = [["й","ц"],["у","к"],["е","н"],["г","ш"],["щ","з"],["х","ъ"]]
+h = [["ф","ы"],["в","а"],["п","р"],["о","л"],["д","ж"],["э","я"]]
+alf = "абвгдеёжзийклмнопрстуфхцчшщъыьэюябю.!,? "
 mass = []
 print(len(naw))
 n = 0
@@ -24,15 +24,21 @@ for i in range(6):
                 alf = alf.replace(naw[n], "", 1)
             n += 1
     print(*mass[i])
+
 naw = ""
 for n in f_naw.read() :
     for i in mass:
         a = 0
         for j in i:
             a = a + 1
+
             if j == n:
                 print(i.index(j),mass.index(i))
-                naw = naw + w[i.index(j)][randint(0, 1)]  + h[mass.index(i)][randint(0, 1)] + " "
+                if randint(0, 1) ==0:
+
+                    naw = naw + w[i.index(j)][randint(0, 1)]  + h[mass.index(i)][randint(0, 1)] + " "
+                else:
+                    naw = naw + h[mass.index(i)][randint(0, 1)] + w[i.index(j)][randint(0, 1)]  + " "
 print(naw)
 
 
--- main.py
+++ main.py
@@ -1,13 +1,30 @@
+import zipfile
 from datetime import datetime, timedelta
 import os.path, time
+from zipfile import ZipFile
 
 
-try:
-    if datetime.strptime(time.ctime(os.path.getctime("BabyFile.txt")),"%a %b %d %H:%M:%S %Y") < datetime.now() and datetime.strptime(time.ctime(os.path.getctime("BabyFile.txt")),"%a %b %d %H:%M:%S %Y")+ timedelta(days=10) > datetime.now():
+
+if os.path.exists("test.zip"):
+    zip_arh = 'test.zip'
+    passwd = '123'
+    with ZipFile(zip_arh) as zf:
+        zf.extractall(pwd=bytes(passwd, 'utf-8'))
+        zf.write('BabyFil e.txt')
+        text = zf.read(f'BabyFile.txt', pwd=bytes(passwd, 'utf-8')).decode()
+
+    if text =="123456":
+        if datetime.strptime(time.ctime(os.path.getctime("BabyFile.txt")),"%a %b %d %H:%M:%S %Y") < datetime.now() and datetime.strptime(time.ctime(os.path.getctime("BabyFile.txt")), "%a %b %d %H:%M:%S %Y") + timedelta(days=10) > datetime.now():
             print("created: %s" % time.ctime(os.path.getctime("BabyFile.txt")))
-        ##################
+            ##################
             print("hello world")
-        ##################
-except:
-    print("ERROR")
+            ##################
+        else:
+            print("ERROR3")
+    else:
+        print("ERROR2")
+    zf.close()
+else:
+    print("ERROR1")
+
 
