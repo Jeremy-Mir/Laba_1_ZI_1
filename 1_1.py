c1bcc74149d7051a191fea357eadc325e5b1f26e
Еремей Миронов <yerem01@mail.ru>
2022-09-22 20:58:26 +0300

    123

--- 1_1.py
+++ 1_1.py
@@ -1,32 +1,32 @@
 from random import randint
 
-f_naw = open('text.txt', 'r',encoding='utf-8')
-naw = input("Введите: ")
+f_new = open('text.txt', 'r', encoding='utf-8')
+new = input("Введите: ")
 w = [["й","ц"],["у","к"],["е","н"],["г","ш"],["щ","з"],["х","ъ"]]
 h = [["ф","ы"],["в","а"],["п","р"],["о","л"],["д","ж"],["э","я"]]
 alf = "абвгдеёжзийклмнопрстуфхцчшщъыьэюябю.!,? "
 mass = []
-print(len(naw))
+print(len(new))
 n = 0
 
 for i in range(6):
-    if n >= len(naw):
+    if n >= len(new):
         mass.append([])
     else:
         mass.append([])
 
     for j in range(6):
-            if n >= len(naw):
+            if n >= len(new):
                 mass[i].append(alf[0])
                 alf = alf[1:]
             else:
-                mass[i].append(naw[n])
-                alf = alf.replace(naw[n], "", 1)
+                mass[i].append(new[n])
+                alf = alf.replace(new[n], "", 1)
             n += 1
     print(*mass[i])
 
-naw = ""
-for n in f_naw.read() :
+new = ""
+for n in f_new.read() :
     for i in mass:
         a = 0
         for j in i:
@@ -36,13 +36,13 @@ for n in f_naw.read() :
                 print(i.index(j),mass.index(i))
                 if randint(0, 1) ==0:
 
-                    naw = naw + w[i.index(j)][randint(0, 1)]  + h[mass.index(i)][randint(0, 1)] + " "
+                    new = new + w[i.index(j)][randint(0, 1)] + h[mass.index(i)][randint(0, 1)] + " "
                 else:
-                    naw = naw + h[mass.index(i)][randint(0, 1)] + w[i.index(j)][randint(0, 1)]  + " "
-print(naw)
+                    new = new + h[mass.index(i)][randint(0, 1)] + w[i.index(j)][randint(0, 1)] + " "
+print(new)
 
 
 f_sif = open('text1.txt', 'w')
-f_sif.write(naw)
-f_naw.close()
+f_sif.write(new)
+f_new.close()
 f_sif.close()
---
+++ 1_2.py
@@ -0,0 +1,12 @@
+f_new = open('text.txt', 'r',encoding='utf-8')
+new = f_new.read()
+fin = ""
+
+n = int(input("Введите n: "))
+for k in range(0,len(new)-1,n*4):
+    for i in range(0,n):
+        fin = fin + new[k + i]
+        fin = fin + new[k + n+1+i*2]
+        fin = fin + new[k + n * 3 + i]
+        fin = fin + new[k + n+i*2]
+print(fin)
