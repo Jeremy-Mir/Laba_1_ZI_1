359e97daab487d9bd4463591ddd2217fee9de30a
Your Name <elik@example.com>
2022-11-10 12:17:17 +0300

    123

--- 2.py
+++ 2.py
@@ -5,7 +5,7 @@ byt = f_new.read()
 n = 0
 
 k  = []
-
+S_block = [[4,10,9,2,13,8,0,14,6,11,1,12,7,15,5,3],[14,11,4,12,6,13,15,10,2,3,8,1,0,7,5,9],[5,8,1,13,10,3,4,2,14,15,12,7,6,0,9,11],[7,13,10,1,0,8,9,15,14,4,6,12,11,2,5,3],[],[],[],[]]
 for i in range(0,8):
     k.append(os.urandom(4))
 for l in k:
@@ -17,15 +17,18 @@ print("   ")
 X = bytearray()
 for n in range(0,4):
     print(byt[n])
+print("   ")
   #  print(byt[n].to_bytes(1, byteorder="big"))
+
+
+
+#for i in range(0,32):
 for n in range(0,4):
 
 
 
-    X.append(byt[n] | k[0][n])
-print("   ")
+    X.append((l_b[n] | k[0][n])%2**32)
 
-for l in X:
 
-    print((l))
+
 f_new.close()
